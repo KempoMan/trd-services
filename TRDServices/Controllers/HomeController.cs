354f81ee884d73fead8f1d4f57ad537fa6711a4a
KempoMan <ajfitzgerald@gmail.com>
2017-08-09 10:29:11 -0700

    Finished most site work, just waiting for smtp creds

--- TRDServices/Controllers/HomeController.cs
+++ TRDServices/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TRDServices.Models;
 
 namespace TRDServices.Controllers
 {
@@ -13,21 +14,55 @@ namespace TRDServices.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult ServicesAdministrative()
+        {
+            return View();
+        }
+
+        public IActionResult ServicesRecruitment()
+        {
+            return View();
+        }
+
+        public IActionResult ServicesTaxPrep()
+        {
+            return View();
+        }
+
+        public IActionResult Contracts()
+        {
+            return View();
+        }
+
+        public IActionResult AboutContact()
+        {
+            return View(new ContactFormViewModel());
+        }
+
+
+        //[HttpPost]
+        //public IActionResult AboutContact(ContactFormViewModel viewModel, string returnUrl = null)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        // work with the viewModel
+        //    }
+        //    // If we got this far, something failed, redisplay form
+        //    return View(viewModel);
+        //}
+
+
+        public IActionResult AboutTerms()
+        {
+            return View();
+        }
+
+        public IActionResult AboutPrivacy()
         {
             return View();
         }
